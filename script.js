24871010ae7fd78468df24bc3307f7e3d3142c99
Buğrahan Erdönmez <erdonmez.bugra@gmail.com>
2026-03-28 12:52:26 +0300

    Add modal functionality for phone selection

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ if (menuToggle && navMenu) {
     });
   });
 }
+
+// MODAL
+let actionType = "call";
+
+function openModal(type) {
+  actionType = type;
+  document.getElementById("phoneModal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("phoneModal").style.display = "none";
+}
+
+function selectPhone(number) {
+  if (actionType === "call") {
+    window.location.href = "tel:+9" + number;
+  } else {
+    window.open(
+      "https://wa.me/9" + number,
+      "_blank"
+    );
+  }
+
+  closeModal();
+}
